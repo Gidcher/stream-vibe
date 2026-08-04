f92e91cd1a8523848694eb1aa3ad0f1ff7ae7ad8
Rustam Rustamov <rustamov.rustam.ruslanovich@gmail.com>
2026-08-04 11:49:32 +0300

    feat: ShowHero section

---
+++ src/pages/show.jsx
@@ -0,0 +1,13 @@
+import ShowHero from "@/sections/ShowHero";
+
+export const metadata = {
+  title: 'Show - Stranger Things',
+}
+
+export default function () {
+  return (
+    <>
+      <ShowHero />
+    </>
+  )
+}
